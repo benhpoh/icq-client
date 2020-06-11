e904c0f1e0d4cb97bf96ecefc558dabc9c145104
Ben P <ben.h.poh@gmail.com>
2020-06-11 21:53:27 +1000

    pre deploy commit

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -10,6 +10,7 @@ export default function Input(props) {
         value={props.message} 
         onChange={evt => props.onChange(evt.target.value)}
         onKeyPress={evt => evt.key === 'Enter' ? props.onSubmit : null }
+        placeholder="Enter your message..."
       />
 
       <button type="submit" className="input-send" onClick={evt => props.sendMessage(evt)}>Send</button>
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,16 +1,44 @@
 import React from "react"
-import Message from "../Message/Message.js"
+import './Messages.css'
 
 export default function Messages({ messages, name }) {
- return (
-   <div>
+  return (
+    <div>
      {messages.map((message, idx) => {
        return (
          <div key={idx}>
            <Message name={name} message={message}/>
          </div>
        )
-     })}
+      })}
    </div>
  )
+}
+ 
+function Message({name, message}) {
+  let messageByCurrentUser = false
+  const trimmedName = name.trim().toLowerCase()
+  if (message.user === trimmedName) {
+    messageByCurrentUser = true
+  }
+
+  return (
+    messageByCurrentUser
+    ? (
+      <div>
+        <h5 className="message-header">You ({message.user}):</h5>
+        <div className="message-wrapper message-wrapper-sent">
+          <p className="message-text">{message.text}</p>
+        </div>
+      </div>
+    )
+    : (
+      <div>
+        <h5 className="message-header">{message.user}: </h5>
+        <div className="message-wrapper message-wrapper-received">
+          <p className="message-text">{message.text}</p>
+        </div>
+      </div>
+    )
+  )
 }
